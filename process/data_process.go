374a1a6e9d16db6b0d190b13b1b9dadb0e2ee186
Mustafa M <neyaadeez@gmail.com>
2024-09-24 16:34:45 -0700

    updated

--- process/data_process.go
+++ process/data_process.go
@@ -36,43 +36,43 @@ func GetProcessedNewJobs() ([]common.JobPosting, error) {
 	onceGetJobs.Do(func() {
 		var allJobs []common.JobPosting
 
-		// jobs, err := GetAllWorkdayJobs()
-		// if err != nil {
-		// 	fmt.Println(err.Error())
-		// 	cachedError = err
-		// 	return
-		// }
-		// fmt.Println("All Workday Jobs: ", len(jobs))
-		// allJobs = append(allJobs, jobs...)
+		jobs, err := GetAllWorkdayJobs()
+		if err != nil {
+			fmt.Println(err.Error())
+			cachedError = err
+			return
+		}
+		fmt.Println("All Workday Jobs: ", len(jobs))
+		allJobs = append(allJobs, jobs...)
 
-		// jobs, err = sites.GetGoogleJobs()
-		// if err != nil {
-		// 	fmt.Println(err.Error())
-		// 	cachedError = err
-		// 	return
-		// }
-		// fmt.Println("All Google Jobs: ", len(jobs))
-		// allJobs = append(allJobs, jobs...)
+		jobs, err = sites.GetGoogleJobs()
+		if err != nil {
+			fmt.Println(err.Error())
+			cachedError = err
+			return
+		}
+		fmt.Println("All Google Jobs: ", len(jobs))
+		allJobs = append(allJobs, jobs...)
 
-		// jobs, err = sites.GetMicrosoftJobs()
-		// if err != nil {
-		// 	fmt.Println(err.Error())
-		// 	cachedError = err
-		// 	return
-		// }
-		// fmt.Println("All Microsoft Jobs: ", len(jobs))
-		// allJobs = append(allJobs, jobs...)
+		jobs, err = sites.GetMicrosoftJobs()
+		if err != nil {
+			fmt.Println(err.Error())
+			cachedError = err
+			return
+		}
+		fmt.Println("All Microsoft Jobs: ", len(jobs))
+		allJobs = append(allJobs, jobs...)
 
-		// jobs, err = sites.GetOracleJobs()
-		// if err != nil {
-		// 	fmt.Println(err.Error())
-		// 	cachedError = err
-		// 	return
-		// }
-		// fmt.Println("All Oracle Jobs: ", len(jobs))
-		// allJobs = append(allJobs, jobs...)
+		jobs, err = sites.GetOracleJobs()
+		if err != nil {
+			fmt.Println(err.Error())
+			cachedError = err
+			return
+		}
+		fmt.Println("All Oracle Jobs: ", len(jobs))
+		allJobs = append(allJobs, jobs...)
 
-		jobs, err := sites.GetAppleJobs()
+		jobs, err = sites.GetAppleJobs()
 		if err != nil {
 			fmt.Println(err.Error())
 			cachedError = err
@@ -80,6 +80,45 @@ func GetProcessedNewJobs() ([]common.JobPosting, error) {
 		}
 		fmt.Println("All Apple Jobs: ", len(jobs))
 		allJobs = append(allJobs, jobs...)
+
+		cachedJobs, cachedError = processDublicateJobs(allJobs)
+		if cachedError != nil {
+			fmt.Println(cachedError.Error())
+		}
+	})
+
+	return cachedJobs, cachedError
+}
+
+func ProcessJobsWithDBForNewlyAddedJobPortal() error {
+	jobs, err := getProcessedNewJobsNewlyAddedJobPortal()
+	if err != nil {
+		fmt.Println("error while processing new jobs: ", err.Error())
+		return err
+	}
+	fmt.Println("Processed Jobs (New Jobs): ", len(jobs))
+
+	err = database.InsertIntoDB(jobs)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func getProcessedNewJobsNewlyAddedJobPortal() ([]common.JobPosting, error) {
+	onceGetJobs.Do(func() {
+		var allJobs []common.JobPosting
+
+		// jobs, err := sites.GetAppleJobs()
+		// if err != nil {
+		// 	fmt.Println(err.Error())
+		// 	cachedError = err
+		// 	return
+		// }
+		// fmt.Println("All Apple Jobs: ", len(jobs))
+		// allJobs = append(allJobs, jobs...)
 
 		cachedJobs, cachedError = processDublicateJobs(allJobs)
 		if cachedError != nil {
