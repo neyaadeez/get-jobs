1912f92ec5144fb7694521c4b5b70ea2adbd6c7a
Mustafa M <neyaadeez@gmail.com>
2024-09-24 16:37:38 -0700

    updated code

--- main.go
+++ main.go
@@ -4,20 +4,24 @@ import (
 	"fmt"
 
 	"github.com/neyaadeez/go-get-jobs/process"
+	"github.com/neyaadeez/go-get-jobs/readme"
 )
 
-func main() {
-
+func processTodaysJobsDBAndReadme() {
 	err := process.ProcessJobsWithDB()
 	if err != nil {
 		fmt.Println(err.Error())
 		return
 	}
 
-	// err = readme.ReadMeProcessNewJobs()
-	// if err != nil {
-	// 	fmt.Println("error while processing readme file with new jobs: ")
-	// }
+	err = readme.ReadMeProcessNewJobs()
+	if err != nil {
+		fmt.Println("error while processing readme file with new jobs: ")
+	}
+}
+
+func main() {
+	processTodaysJobsDBAndReadme()
 
 	// workday.Init()
 	// resp, err := sites.GetAppleJobs()
