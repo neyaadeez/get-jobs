a2534b3889ad2d3ae33eba4a3b74fc283c06b965
Mustafa M <neyaadeez@gmail.com>
2024-10-14 18:50:41 -0700

    updated today's jobs

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/neyaadeez/go-get-jobs/common"
 	"github.com/neyaadeez/go-get-jobs/process"
 	"github.com/neyaadeez/go-get-jobs/readme"
 )
@@ -22,8 +21,8 @@ func processTodaysJobsDBAndReadme() {
 }
 
 func main() {
-	//processTodaysJobsDBAndReadme()
-	process.ProcessJobsWithDBForNewlyAddedJobPortal(common.NetApp, false)
+	processTodaysJobsDBAndReadme()
+	//process.ProcessJobsWithDBForNewlyAddedJobPortal(common.NetApp, false)
 
 	// workday.Init()
 	// resp, err := sites.GetNetAppJobs()
